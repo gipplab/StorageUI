05f2418b3185e4508ffe0be05201ac66925bd0b6
iivooo <ivoschueepp@gmail.com>
2018-02-14 03:14:55 +0000

    fixed register, added image

--- routes/register.js
+++ routes/register.js
@@ -16,6 +16,7 @@ router.get('/', function (req, res, next) {
     if (req.session.loggedin === true) {
         res.redirect('/')
     } else {
+        console.log(req.session.regErrs)
         res.render('register', {loggedin: req.session.loggedin, register: true, errors: req.session.regErrs}); //content
         req.session.regErrs = null;
     }
@@ -25,11 +26,12 @@ router.post('/submit', async function (req, res, next) {
     try {
         var errs = await loginHelper.checkRegisterSubmit(req, sha256(req.body.username),
             sha256(req.body.password), sha256(req.body.passwordrep))
+        console.log(errs)
         if (errs) {
             req.session.regErrs = errs;
             res.redirect('/register');
         } else {
-            await loginHelper.userToDb(sha256(req.body.username), sha256(req.body.password))
+            await loginHelper.userToDb(req, sha256(req.body.username), sha256(req.body.password))
             req.session.loggedin = true;
             req.session.userSha = sha256(req.body.username)
             res.redirect('/add/article');
